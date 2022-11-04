9cf06cbef6bf705f05fde2bb2ba59cef09cd9724
alexelisenko <39712468+alexelisenko@users.noreply.github.com>
2022-11-04 18:47:28 -0400

    darwin method stubs

--- tproxy_darwin_stub.go
+++ tproxy_darwin_stub.go
@@ -13,3 +13,11 @@ func ListenTCP(network string, laddr *net.TCPAddr) (net.Listener, error) {
 func ListenUDP(network string, laddr *net.UDPAddr) (*net.UDPConn, error) {
 	return nil, nil
 }
+
+func ReadFromUDP(conn *net.UDPConn, b []byte) (int, *net.UDPAddr, *net.UDPAddr, error) {
+	return 0, nil, nil, nil
+}
+
+func DialUDP(network string, laddr *net.UDPAddr, raddr *net.UDPAddr) (*net.UDPConn, error) {
+	return nil, nil
+}
